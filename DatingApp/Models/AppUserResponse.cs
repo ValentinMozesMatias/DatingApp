b660cb86ad8a0da124e77c3fda67b9baffb6309d
Valentin Matias Mozes <valentin.mozes@firstbank.ro>
2023-06-29 17:11:53 +0300

    Controller CRUD implemented and Migration name changed

---
+++ DatingApp/Models/AppUserResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace DatingApp.Models
+{
+    public class AppUserResponse<T>
+
+           where T : class
+    {
+        public AppUserResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+        public bool Success { get; set; }
+
+        public List<string> ErrorMessages { get; set; }
+
+        public T Data { get; set; }
+    }
+}
