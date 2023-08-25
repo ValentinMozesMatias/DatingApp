f8b23d6b0ab37d60cfa2fae027921ff9ea401d68
Valentin Matias Mozes <valentin.mozes@firstbank.ro>
2023-08-25 18:09:25 +0300

    Implemented Password Hash, Salt, JWT Token, DTO's, Controllers and TokenService

---
+++ DatingApp/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using DatingApp.Data;
+using DatingApp.DTOs;
+using DatingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+using System.Transactions;
+
+namespace DatingApp.Controllers
+{
+    public class AccountController: BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")] // POST: api/account/register
+
+        public async Task<ActionResult<AppUser>> Register([FromBody]RegisterDTO registerDTO)
+        {
+            if (await UserExists(registerDTO.UserName)) return BadRequest("Username is taken");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDTO.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login([FromBody]LoginDTO loginDTO)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == loginDTO.UserName);
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            for (int i = 0 ; i < computedHash[i]; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return Ok(user);
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+        
+    }
+}
---
+++ DatingApp/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [Controller]
+    public class BaseApiController :ControllerBase
+    {
+
+    }
+}
